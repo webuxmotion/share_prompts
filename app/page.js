2aa2a8a2e31603da2957418d51f09ab35576bc25
webuxmotion <pereverziev.adnrii@gmail.com>
2024-06-23 22:17:08 +0300

    Metadata

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: "Home page"
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+export async function generateMetadata({ params, searchParams }) {
+  const postId = params.postId;
+
+  return {
+    title: `Post id is: ${postId}`
+  }
+}
+
 export default function page({ params }) {
   return (
     <div>Page id: {params.postId}</div>
